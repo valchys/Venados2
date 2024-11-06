d7392adf7e742b28e4beac2ba8446f9f2d462333
Valeria Corrales Hoyos <104111101+valchys@users.noreply.github.com>
2024-11-05 23:24:58 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_local' );
+define( 'DB_NAME', 'wordpress_aws' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'userWP' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'passwordWP' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '172.31.31.13' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
